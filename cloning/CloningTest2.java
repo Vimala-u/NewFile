607d4e12dfb9f42bd7bc5d30bf24deaafce9114f
vishnuramireddy <vishnuramireddy283@gmail.com>
2021-01-26 15:23:56 +0530

    Added clonning file

---
+++ cloning/CloningTest2.java
@@ -0,0 +1,36 @@
+package cloning;
+
+public class CloningTest2 {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+
+		// create emp obj with data
+		Employee e1 = new Employee();// object
+		e1.setId(1);
+		e1.setName("shyam");
+
+		// clone emp obj
+		Employee e2 = (Employee) e1.clone();
+
+		System.out.println("showing e1");
+		System.out.println(e1);
+
+		System.out.println("showing e2");
+		System.out.println(e2);
+
+		System.out.println("************changing e1 *******************");
+		e1.setId(2);
+		e1.setName("kumar");
+
+		System.out.println("showing e1");
+		System.out.println(e1);
+
+		System.out.println("showing e2");
+		System.out.println(e2);
+
+		System.out.println("git hub");
+		System.out.println("showing e");
+		System.out.println("showing e78");
+	}
+
+}
